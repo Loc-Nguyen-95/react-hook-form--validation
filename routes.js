a9cf36cda327f8dcd3d35fb95f0bab571b5a096b
Loc-Nguyen-95 <locnpFX13001@funix.edu.vn>
2023-06-19 15:17:26 +0700

    second-commit

--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
 const requestHandler = (req, res) => {
+  // console.log(req)
+
   const url = req.url;
   const method = req.method;
+
+  //Controller 1
   if (url === '/') {
+
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write(
@@ -13,28 +18,42 @@ const requestHandler = (req, res) => {
     return res.end();
   }
   
+  //Controller 2 
   if (url === '/message' && method === 'POST') {
+
     const body = [];
+
     req.on('data', chunk => {
       console.log(chunk);
       body.push(chunk);
     });
-    return req.on('end', () => {
+
+    //return :  Để chắc là thực hiện req.on..'end' trước khi thực hiện final cope : my first page 
+    req.on('end', () => {
+
       const parsedBody = Buffer.concat(body).toString();
+
       const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
+
+      fs.writeFile('message.txt', message, err => { // writeFile : không chặn 
+        // redirect về home '/' (index page) khi có error 
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
       });
+
     });
   }
+
+  // nếu không có return phía trên -> throw new ERR_HTTP_HEADERS_SENT('set')
+  // can not set header sau khi sent to client (return res.end() phía trên )
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
+  
 };
 
 exports.handler = requestHandler;
